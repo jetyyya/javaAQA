cf5de540a01f8a961442a926d9423f934a932939
jetyyya <jetyyya@gmail.com>
2023-06-10 18:29:44 +0300

    Second commit HW25 Added: Correction Homework for lesson 25. Moved logic and output in different methods.

--- HW26/src/main/java/com/alevel/operators/HW26GameOfTheCity.java
+++ HW26/src/main/java/com/alevel/operators/HW26GameOfTheCity.java
@@ -5,6 +5,7 @@ public class HW26GameOfTheCity {
     public static void main(String[] args) {
         String[] cities = generateCitiesArray();
         playCitiesGame(cities);
+        System.out.println("Гра закінчена");
     }
 
     public static String[] generateCitiesArray() {
@@ -21,43 +22,80 @@ public class HW26GameOfTheCity {
     public static void playCitiesGame(String[] cities) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Давайте пограємо у «Міста». Для початку треба ввести назву будь-якого міста. Ваш хід!");
-        System.out.println("Граємо українською. Ваш хід!");
+        printGameInstructions();
 
         String lastCity = scanner.nextLine();
         char lastLetter = Character.toUpperCase(lastCity.charAt(lastCity.length() - 1));
 
         while (true) {
-            boolean foundCity = false;
-            for (int i = 0; i < cities.length; i++) {
-                String city = cities[i];
-                if (Character.toUpperCase(city.charAt(0)) == lastLetter) {
-                    System.out.println("Місто: " + city);
-                    lastLetter = Character.toUpperCase(city.charAt(city.length() - 1));
-                    cities[i] = "used";
-                    foundCity = true;
-                    break;
-                }
-            }
-
-            if (!foundCity) {
-                System.out.println("Комп'ютер не знайшов жодного міста на літеру " + "'" + Character.toLowerCase(lastLetter) + "'" + ", та програв");
+            String city = findCity(cities, lastLetter);
+            if (city == null) {
+                printComputerLost(lastLetter);
                 break;
             }
+            printCity(city);
+            lastLetter = Character.toUpperCase(city.charAt(city.length() - 1));
+            removeCity(cities, city);
 
-            System.out.println("Ваш хід: (або введіть 'exit', щоб завершити гру)");
-            String playerCity = scanner.nextLine();
+            String playerCity = getPlayerCity(scanner);
             if (playerCity.equalsIgnoreCase("exit")) {
                 break;
             }
             char firstLetter = Character.toUpperCase(playerCity.charAt(0));
             if (firstLetter != lastLetter) {
-                System.out.println("Введена назва міста не починається на літеру " + "'" + Character.toLowerCase(lastLetter) + "'");
-                System.out.println("Ви програли!");
+                printInvalidCityInput(lastLetter);
                 break;
             }
             lastLetter = Character.toUpperCase(playerCity.charAt(playerCity.length() - 1));
         }
-        System.out.println("Гра закінчена");
+    }
+
+    public static String findCity(String[] cities, char lastLetter) {
+        for (String city : cities) {
+            if (Character.toUpperCase(city.charAt(0)) == lastLetter) {
+                return city;
+            }
+        }
+        return null;
+    }
+
+    public static void removeCity(String[] cities, String city) {
+        for (int i = 0; i < cities.length; i++) {
+            if (cities[i].equals(city)) {
+                cities[i] = "used";
+                break;
+            }
+        }
+    }
+
+    public static void printGameInstructions() {
+        System.out.println("Давайте пограємо у «Міста». Для початку треба ввести назву будь-якого міста.");
+        System.out.println("Граємо українською. Ваш хід!");
+    }
+
+    public static void printCity(String city) {
+        System.out.println("Місто: " + city);
+    }
+
+    public static void printComputerLost(char lastLetter) {
+        System.out.println("Комп'ютер не знайшов жодного міста на літеру " + "'" + Character.toLowerCase(lastLetter) + "'" + ", та програв");
+    }
+
+    public static String getPlayerCity(Scanner scanner) {
+        System.out.println("Ваш хід: (або введіть 'exit', щоб завершити гру)");
+        String input = " ";
+        do {
+            input = scanner.nextLine();
+            if (input.isEmpty()) {
+                System.out.println("Пустий ввід! Введіть назву міста.");
+            }
+        } while (input.isEmpty());
+        return input;
+
+    }
+
+    public static void printInvalidCityInput(char lastLetter) {
+        System.out.println("Введена назва міста не починається на літеру " + "'" + Character.toLowerCase(lastLetter) + "'");
+        System.out.println("Ви програли!");
     }
 }
